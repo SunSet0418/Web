ed8813742733f7d22bf4d2f1655b19c2fe6bc680
SunSet0418 <cds07016@naver.com>
2016-11-10 20:48:28 +0900

    4sodae list

--- 4sodae/app.js
+++ 4sodae/app.js
@@ -1,14 +1,15 @@
 var express = require('express');
+var app = express();
 var mongoose = require('mongoose')
-var bodyparser = require('body-parser')
+var bodyParser = require('body-parser')
 var schema = mongoose.Schema;
 
 app.use(bodyParser.urlencoded({
-  extended: true
-}))
+    extended: true
+}));
 
-app.listen(80, function(){
-  console.log("Server Running at 80 Port")
+app.listen(3000, function(){
+  console.log("Server Running at 3000 Port")
 })
 
 mongoose.connect("mongodb://localhost/4sodae", function(err){
@@ -29,7 +30,7 @@ var UserSchema = new schema({
 
 var User = mongoose.model('user', UserSchema);
 
-app.get('/', function(req, res){
+app.post('/login', function(req, res){
   User.findOne({
     password: req.param('password')
   }, function(err, result){
@@ -40,18 +41,48 @@ app.get('/', function(req, res){
     if(result){
       console.log(result.name+" Login")
       res.json({
-        success: true,
-        message: "Login success"
+          success: true,
+          name: result.name
       })
-      res.redirect('/list.html')
     }
     else{
       res.json({
         success: false,
         message: "user not Founded"
       })
-      res.redirect('/')
     }
   }
 )
+})
+
+app.post('/register', function(req, res){
+  var name  = req.param('name');
+  var password = req.param('password')
+
+  user = new User({
+    name: req.param('name'),
+    password: req.param('password')
+  })
+
+  user.save(function(err){
+    if(err){
+      console.log("User save error")
+      throw err
+    }
+    else{
+      console.log("user save success")
+      res.json({
+        success: true,
+        message: "회원가입 성공"
+      })
+    }
+  })
+})
+
+app.get('/list.html', function(req, res){
+  res.redirect('index.html')
+})
+
+app.get('/', function(req, res){
+  res.send("adsfa")
 })
